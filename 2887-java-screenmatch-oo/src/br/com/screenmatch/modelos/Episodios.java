eda7a72a1b0fe90289b40f2b466ebb364d130628
Jose Valim <joseaugustovalim1994@gmail.com>
2024-06-16 14:20:31 -0300

    feat: Filtro recomendacao

--- 2887-java-screenmatch-oo/src/br/com/screenmatch/modelos/Episodios.java
+++ 2887-java-screenmatch-oo/src/br/com/screenmatch/modelos/Episodios.java
@@ -1,9 +1,13 @@
 package br.com.screenmatch.modelos;
 
-public class Episodios {
+import br.com.screenmatch.calculos.Classificavel;
+
+public class Episodios implements Classificavel {
     private Integer numero;
     private String nome;
     private Serie serie;
+    private Integer totalVisualizacoes;
+
 
     public Integer getNumero() {
         return numero;
@@ -27,5 +31,22 @@ public class Episodios {
 
     public void setSerie(Serie serie) {
         this.serie = serie;
+    }
+
+    public Integer getTotalVisualizacoes() {
+        return totalVisualizacoes;
+    }
+
+    public void setTotalVisualizacoes(Integer totalVisualizacoes) {
+        this.totalVisualizacoes = totalVisualizacoes;
+    }
+
+    @Override
+    public int getClassificao() {
+        if (totalVisualizacoes > 100) {
+            return 4;
+        } else {
+            return 2;
+        }
     }
 }
