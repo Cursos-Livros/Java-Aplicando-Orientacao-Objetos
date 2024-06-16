9344adf1acc000f47c4c1b38e3b21f4b398ff37e
Jose Valim <joseaugustovalim1994@gmail.com>
2024-06-16 11:39:41 -0300

    fix: modelo e calculos

---
+++ 2887-java-screenmatch-oo/src/br/com/screenmatch/modelos/Episodios.java
@@ -0,0 +1,31 @@
+package br.com.screenmatch.modelos;
+
+public class Episodios {
+    private Integer numero;
+    private String nome;
+    private Serie serie;
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+}
